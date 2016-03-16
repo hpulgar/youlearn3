c52312182e1efe29e253ce2ae6f97871e2e1dfb6
Felipe <Felipe@DESKTOP-8EDOAED>
2016-03-16 17:24:34 -0300

    Jquery modificado, no muestra el boton responder al hacer click.

---
+++ src/java/ENTITIES/Bean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ENTITIES;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Bean {
+    
+    private Boolean renderInputText;
+
+public void setRenderInputText(Boolean renderInputText) {
+    this.renderInputText = renderInputText;
+}
+
+public Boolean getRenderInputText() {
+   return renderInputText;
+}
+    
+}
